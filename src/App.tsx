7b2dbedcf17523c97016e6bafd80d72c2ae639a3
Constantin-dot <konstantinkspb@gmail.com>
2020-05-31 20:44:22 +0300

    firstTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.modul.css';
+import {StateType} from './index';
 
-function App() {
+
+type PropsType = {
+    state: StateType
+}
+
+
+const App: React.FC<PropsType> = (props) => {
   return (
     <div className={"App"}>
       <div className={"ava"}>
         <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSz3oe7au6Voj7bwkAdVqEhbemBlvOSDNsybGBb-1nN_q4gPhB3&usqp=CAU"} alt={"ava"}></img>
       </div>
       <div className={"messageBox"}>
-        <div className={"autorName"}>Constantin</div>
-        <div className={"message"}>Hi!What's up?</div>
-        <div className={"time"}>15:23</div>
+        <div className={"authorName"}>{props.state.name}</div>
+        <div className={"message"}>{props.state.message}</div>
+        <div className={"time"}>{props.state.time}</div>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+export type StateType = {
+    name: string
+    message: string
+    time: string
+}
+
+let state: StateType = {
+    name: "Constantin",
+    message: "Hi!What's up?",
+    time: "15:23"
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App state={state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
