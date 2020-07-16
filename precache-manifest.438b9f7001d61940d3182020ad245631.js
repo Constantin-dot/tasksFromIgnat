f1b5b39120c2d676b765e5759be11b99452299ae
Constantin-dot <konstantinkspb@gmail.com>
2020-07-16 18:24:34 +0300

    Updates

---
+++ precache-manifest.438b9f7001d61940d3182020ad245631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d218b30145fc304b880adbfd2f59499f",
+    "url": "/tasksFromIgnat/index.html"
+  },
+  {
+    "revision": "7a7b5cd1a85ed3d73bc1",
+    "url": "/tasksFromIgnat/static/css/main.437f100e.chunk.css"
+  },
+  {
+    "revision": "c1b9cdeb50522899fbc9",
+    "url": "/tasksFromIgnat/static/js/2.beafc187.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tasksFromIgnat/static/js/2.beafc187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7b5cd1a85ed3d73bc1",
+    "url": "/tasksFromIgnat/static/js/main.0d75ef1e.chunk.js"
+  },
+  {
+    "revision": "ed55642cc6365fe2b27a",
+    "url": "/tasksFromIgnat/static/js/runtime-main.9d0a3fc4.js"
+  }
+]);
