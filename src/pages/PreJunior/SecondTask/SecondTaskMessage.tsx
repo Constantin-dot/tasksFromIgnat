accdc4fcbbc24b219d60be005e9d2dbcedf22705
Constantin-dot <konstantinkspb@gmail.com>
2020-07-27 14:42:23 +0300

    fixed callback function at unibutton in second task

--- src/pages/PreJunior/SecondTask/SecondTaskMessage.tsx
+++ src/pages/PreJunior/SecondTask/SecondTaskMessage.tsx
@@ -3,6 +3,7 @@ import classes from './SecondTask.module.css';
 import {CasesType, StateType} from './SecondTask';
 import {UniCheckbox} from "../../../common/UniCheckbox";
 import {DeleteButton} from "../../../common/DeleteButton";
+import {UniButton} from "../../../common/UniButton";
 // import {UniButton} from "../../../common/UniButton";
 
 type PropsType = {
@@ -42,27 +43,27 @@ function SecondTaskMessage(props: PropsType)  {
                 </div>
                 <div>
 
-                    {/*<UniButton onClick={props.changeCases}*/}
-                    {/*           value={"low"}*/}
-                    {/*/>*/}
-                    {/*<UniButton onClick={props.changeCases}*/}
-                    {/*           value={"middle"}*/}
-                    {/*/>*/}
-                    {/*<UniButton onClick={props.changeCases}*/}
-                    {/*           value={"high"}*/}
-                    {/*/>*/}
-                    {/*<UniButton onClick={props.changeCases}*/}
-                    {/*           value={"highest"}*/}
-                    {/*/>*/}
-                    {/*<UniButton onClick={props.changeCases}*/}
-                    {/*           value={"active"}*/}
-                    {/*/>*/}
-                    {/*<UniButton onClick={props.changeCases}*/}
-                    {/*           value={"completed"}*/}
-                    {/*/>*/}
-                    {/*<UniButton onClick={props.changeCases}*/}
-                    {/*           value={"all"}*/}
-                    {/*/>*/}
+                    <UniButton onClick={() => props.changeCases("low")}
+                               value={"low"}
+                    />
+                    <UniButton onClick={() => props.changeCases("middle")}
+                               value={"middle"}
+                    />
+                    <UniButton onClick={() => props.changeCases("high")}
+                               value={"high"}
+                    />
+                    <UniButton onClick={() => props.changeCases("highest")}
+                               value={"highest"}
+                    />
+                    <UniButton onClick={() => props.changeCases("active")}
+                               value={"active"}
+                    />
+                    <UniButton onClick={() => props.changeCases("completed")}
+                               value={"completed"}
+                    />
+                    <UniButton onClick={() => props.changeCases("all")}
+                               value={"all"}
+                    />
                 </div>
                 <div className={classes.time}>{props.state.time}</div>
             </div>
